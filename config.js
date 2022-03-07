9b75f549701f3ed17311c96e6f58dc3be369ffcc
Nguyễn Phúc Khang <90046327+npk72@users.noreply.github.com>
2022-03-07 21:18:55 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'YOU';
+const giftUrl = 'https://youtu.be/8CEJoCr_9UI';
+const eventName = 'FOR';
+const titleCard = 'Happy 8/3';
+const contentCard = 'Always keep your smile ';
+
+
+const giftImage = 'flower.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
